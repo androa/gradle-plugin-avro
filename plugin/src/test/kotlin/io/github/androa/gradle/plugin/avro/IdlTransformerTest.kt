d811d27ee419014454b4812ac873b91997e25151
André Roaldseth <andre@roaldseth.net>
2025-04-07 22:36:53 +0200

    chore(test): Make the IDL transformer test use File directly

--- plugin/src/test/kotlin/io/github/androa/gradle/plugin/avro/IdlTransformerTest.kt
+++ plugin/src/test/kotlin/io/github/androa/gradle/plugin/avro/IdlTransformerTest.kt
@@ -2,14 +2,14 @@ package io.github.androa.gradle.plugin.avro
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import kotlin.io.path.Path
+import java.io.File
 
 class IdlTransformerTest {
     @Test
     fun `test IDL files are transformed to protocols`() {
         val idlTransformer = IdlTransformer()
-        val inputFiles = setOf(Path("src/test/resources/idl").toFile())
-        val outputDir = Path("build/intermediates/avro/").toFile()
+        val inputFiles = setOf(File("src/test/resources/idl/idl-schema.avdl"))
+        val outputDir = File("build/intermediates/avro/")
 
         idlTransformer.transformIdl(inputFiles, outputDir)
 
